387ae4163c380bb8966420a9641d235f07bcd02a
bear <boundless.forest@outlook.com>
2022-04-22 18:20:01 +0800

    Get total pairs

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,28 @@ const { ApiPromise } = require('@polkadot/api');
 const { HttpProvider } = require('@polkadot/rpc-provider');
 const { xxhashAsHex } = require('@polkadot/util-crypto');
 
-const provider = new HttpProvider('http://127.0.0.1:9933')
-// const provider = new HttpProvider('https://crab-rpc.darwinia.network');
+// const provider = new HttpProvider('http://127.0.0.1:9933')
+const provider = new HttpProvider('https://pangolin-rpc.darwinia.network');
 
 main();
 
 async function main() {
     let api = await ApiPromise.create({ provider });
 
-    let all_runtime_pallets: Array<any> = new Array();
     let prefixes: Array<any> = new Array();
     
     let metadata = await api.rpc.state.getMetadata();
     let metadata_pallets = metadata.asLatest.pallets;
 
-    let total_pairs;
-    metadata_pallets.forEach( async pallet => {
+    let total_pairs = 0;
+    for (let pallet of metadata_pallets) {
         let prefix = xxhashAsHex(pallet.name, 128);
         prefixes.push(prefix);
 
         let pairs = await cal_single_pallet(prefix);        
+        console.log("The " + pallet.name + "pallet has " + pairs + " keys");
         total_pairs += pairs;
-    });
+    }
 
     console.log("total pairs: ", total_pairs);
 }
